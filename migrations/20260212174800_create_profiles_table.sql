960cecc342d665b160e866a81187f1986bc9cfae
Jake Wray <jakewray@mailbox.org>
2026-03-13 01:02:38 -0700

    fix: change `last_updated` column type from `BIGINT` to `INTEGER` in user and group profiles tables

--- migrations/20260212174800_create_profiles_table.sql
+++ migrations/20260212174800_create_profiles_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_profiles (
     user_id TEXT PRIMARY KEY,
     encrypted_blob BLOB NOT NULL,
-    last_updated BIGINT NOT NULL,
+    last_updated INTEGER NOT NULL,
     version INTEGER DEFAULT 1
 );
--- migrations/20260312170557_create_group_profiles_table.sql
+++ migrations/20260312170557_create_group_profiles_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS group_profiles (
     group_id TEXT PRIMARY KEY,
     encrypted_blob BLOB NOT NULL,
-    last_updated BIGINT NOT NULL,
+    last_updated INTEGER NOT NULL,
     version INTEGER DEFAULT 1
 );
